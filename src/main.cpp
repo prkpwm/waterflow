33997fba8c1ca5f6a44c1cee069c2bf259e2b578
prkpwm <prkpwm.dev@gmail.com>
2023-07-21 18:48:25 +0700

    time

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 
 const int RELAY_PIN = D8;
 const int SOIL_MOISTURE_PIN = D4;
-const int WORKING_TIME_THRESHOLD = 120000;
+const int WORKING_TIME_THRESHOLD = 80000;
 
 unsigned long workingStartTime = 0;
 
-int currentRelayState = LOW;
+int currentRelayState = HIGH;
+
+int lastSoilMoisture = -1;
 
 // Function declarations
 void relayHandler(int relayPin, int relayState);
@@ -15,21 +17,19 @@ void setup()
 {
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(SOIL_MOISTURE_PIN, INPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  relayHandler(RELAY_PIN, HIGH);
 }
 
 void loop()
 {
   int soilMoisture = digitalRead(SOIL_MOISTURE_PIN);
 
-  if (Serial.available() > 0)
-  {
-    soilMoisture = Serial.read();
-  }
+  Serial.println(soilMoisture);
 
   if (soilMoisture == LOW)
   {
-    relayHandler(RELAY_PIN, HIGH);
+    relayHandler(RELAY_PIN, LOW);
     workingStartTime = millis();
   }
   else if (millis() - workingStartTime < WORKING_TIME_THRESHOLD)
@@ -38,13 +38,12 @@ void loop()
   }
   else
   {
-    relayHandler(RELAY_PIN, LOW);
+    relayHandler(RELAY_PIN, HIGH);
   }
 
   Serial.println(currentRelayState);
 
-
-  delay(100);
+  delay(1000);
 }
 
 void relayHandler(int relayPin, int relayState)
